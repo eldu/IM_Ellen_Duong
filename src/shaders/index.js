64e9d511fa52305c8d72302c1a125eeebe283ee0
Ellen Duong <ellen@levy-ve502-0567.apn.wlan.upenn.edu>
2018-05-03 07:30:25 -0400

    Moved project

---
+++ src/shaders/index.js
@@ -0,0 +1,7 @@
+
+// This file exports available shaders to the GUI.
+// follow this syntax to make your shaders available to the GUI
+export {default as Lambert} from './lambert'
+export {default as ToonShader} from './toonshader'
+// export {default as Iridescent} from './iridescent'
+export {default as Noise} from './noise'
